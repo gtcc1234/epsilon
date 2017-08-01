f707cb13899f4a2ce6fe91934d3d8da9fc436e60
GTC <ger@GTCs-MacBook-Pro.local>
2017-08-02 00:46:25 +0800

    change store

--- src/Store/index.js
+++ src/Store/index.js
@@ -7,10 +7,10 @@ export const store = new Vuex.Store({
   state: {
     loadedContents: [
       {
-        imageUrl: 'https://bitcoin.org/img/icons/opengraph.png', id: '1', title: 'Trying Content 1', date: '2017-07-17', link: 'https://bitcoin.org'
+        imageUrl: 'https://bitcoin.org/img/icons/opengraph.png', id: '1', title: 'Bitcoin', date: '2017-07-17', link: 'https://bitcoin.org'
       },
       {
-        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqL_dAHmx6dx9W1ITcKJFryZEuLJYavVHJuDDiXu6lq85db8fMtQ', id: 'aaaa41231123aa2', title: 'Trying Content 2', date: '2017-07-19', link: 'https://www.ethereum.org'
+        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqL_dAHmx6dx9W1ITcKJFryZEuLJYavVHJuDDiXu6lq85db8fMtQ', id: 'aaaa41231123aa2', title: 'Etherium', date: '2017-07-19', link: 'https://www.ethereum.org'
       },
       {
         imageUrl: 'http://technode.com/wp-content/uploads/2015/12/technode-logo-ob-200.png', id: '3', title: 'Technode', date: '2017-07-04', link: 'http://technode.com/2017/07/20/ant-financial-invests-shanghai-based-fintech-startup-vfinance/'
